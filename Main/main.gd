922904652902f0e43646c17f1be5ab3dcde95659
Anastylos <157839647+Anastylos@users.noreply.github.com>
2026-03-16 11:17:53 +0100

    dialog working, but not pausing the game

--- Main/main.gd
+++ Main/main.gd
@@ -1,5 +1,18 @@
 extends Node3D
-@onready var dialog_player : DialogPlayer = $DialogPlayer
+
+@onready var dialog_player: DialogPlayer = $DialogPlayer
+@onready var player: CharacterBody3D = $Player
 
 func _ready() -> void:
+	# Signale verbinden
+	dialog_player.dialog_started.connect(_on_dialog_started)
+	dialog_player.dialog_ended.connect(_on_dialog_ended)
+
+	# Starte den Dialog
 	dialog_player.start()
+
+func _on_dialog_started() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_dialog_ended() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
