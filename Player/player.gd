921a70ffa90cd82412ca021e46b917f225af81d1
Anastylos <157839647+Anastylos@users.noreply.github.com>
2026-04-09 09:16:50 +0200

    commented out death dialog, was not working

--- Player/player.gd
+++ Player/player.gd
@@ -23,7 +23,7 @@ signal died()
 @onready var attack_controller: AttackController = $AttackController
 @onready var pause_menu: PauseMenu = $"../PauseMenu"
 
-@onready var dialog_player: DialogPlayer = $DialogPlayer
+#@onready var dialog_player: DialogPlayer = $DialogPlayer
 
 @onready var timestamp:= str(Time.get_ticks_msec()) #string needed for talo
 
@@ -245,22 +245,22 @@ func die() -> void:
 	died.emit()
 	print("Player died")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	death_dialog()
+	#death_dialog()
 	get_tree().reload_current_scene()
 
 	
-func death_dialog() -> void:
-	# Signale verbinden
-	dialog_player.dialog_started.connect(_on_dialog_started)
-	dialog_player.dialog_ended.connect(_on_dialog_ended)
-	# Starte den Dialog
-	dialog_player.start()
-	
-func _on_dialog_started() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-
-func _on_dialog_ended() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Talo.events.track("dialog_end", {"time": timestamp})
-	Talo.events.flush()
+#func death_dialog() -> void:
+	## Signale verbinden
+	#dialog_player.dialog_started.connect(_on_dialog_started)
+	#dialog_player.dialog_ended.connect(_on_dialog_ended)
+	## Starte den Dialog
+	#dialog_player.start()
+	#
+#func _on_dialog_started() -> void:
+	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+#
+#
+#func _on_dialog_ended() -> void:
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Talo.events.track("dialog_end", {"time": timestamp})
+	#Talo.events.flush()
